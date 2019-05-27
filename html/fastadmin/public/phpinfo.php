d946710e98bea6257b0a4aafffac37f5a7d3608a
董佳林 <2496567562@qq.com>
2019-05-27 09:46:57 +0800

    删除

--- html/fastadmin/public/phpinfo.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: lin
- * Date: 2019/2/12
- * Time: 5:38 PM
- */
-phpinfo();
--- html/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-//echo 'hello php!';
-//phpinfo();
-//die;
-$redis = new redis();
-$redis->connect('redis', 6379);
-echo "Connection to server sucessfully";
-////查看服务是否运行
-//echo "Server is running: " . $redis->ping();
